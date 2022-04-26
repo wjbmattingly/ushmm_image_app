a50464d539778cbbe00b2cd26963f71b2fbbea0a
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2022-04-26 07:08:07 -0400

    init

--- app.py
+++ app.py
@@ -3,29 +3,27 @@ import glob
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model
+import tensorflow as tf
+from PIL import Image
+from operator import itemgetter
+import pandas as pd
 
-st.title("USHMM Image Processing Pipeline")
 
-# @st.cache(allow_output_mutation=True)
-# def load_my_model():
-#     MODEL_PATH = "models/01_image_type"
-#     model = load_model(MODEL_PATH)
-#     model._make_predict_function()
-#     model.summary()  # included to make it visible when model is reloaded
-#     return model
 
-# model = load_my_model()
 
-MODEL_PATH = "models/01_image_type"
-model = load_model(MODEL_PATH)
+@st.cache(allow_output_mutation=True)
+def load_my_model():
+    MODEL_PATH = "models/01_image_type_15"
+    model = load_model(MODEL_PATH)
 
-def test_model(classes, file, threshold):
-    heldout_files = glob.glob(heldout_files)
+    return model
+
+def test_model(classes, file, threshold, model):
     image_size = (224, 224)
     i=0
-    results = {"unknown": []}
-    for c in classes:
-        results[c] = []
+    results = []
+#     for c in classes:
+#         results[c] = 0
     img = load_img(file, target_size=image_size)
 
     img_array = img_to_array(img)
@@ -33,17 +31,35 @@ def test_model(classes, file, threshold):
 
     predictions = model.predict(img_array)
     scores = predictions[0]
-
+#     st.write(scores)
     found = False
     i=0
     for score in scores:
         if score > threshold:
-            results[classes[i]].append(img)
+            results.append((classes[i], score))
             found=True
         i=i+1
     if found == False:
-        results["unknown"].append(img)
+        results.append(("unknown", 1))
     print ("Complete")
-    for item in results:
-        print (item, len(results[item]))
+    results = sorted(results, key=itemgetter(1))
+    results.reverse()
     return results
+model = load_my_model()
+st.title("USHMM Image Processing Pipeline")
+image = st.file_uploader("Load your Image")
+
+if st.button("Run"):
+    image = Image.open(image)
+
+    col1, col2 = st.columns(2)
+    col1.image(image, caption='Your Image')
+    image = image.convert("RGB")
+    image.save("temp_image.jpg")
+
+
+    classes = ['document', 'passport', 'photograph']
+
+
+    results = test_model(classes, "temp_image.jpg", .0, model)
+    col2.write(pd.DataFrame(results, columns=["class", "score"]))
